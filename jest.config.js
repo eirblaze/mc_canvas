94b6f5d357b8d1a7b01315cc797984a64debc968
eirblaze <eirblaze@live.jp>
2019-06-10 01:23:44 +0900

    設定ファイル移動

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,14 @@
 // @see [この頃流行りのJestを導入して軽快にJSをテストしよう](https://qiita.com/hogesuke_1/items/8da7b63ff1d420b4253f)
 module.exports = {
 	verbose: true,
-	modulePaths: [], // NODE_PATH 環境変数を設定する代わりの API
 	transform: {
 		"^.+\\.js$": "<rootDir>/node_modules/babel-jest" // <rootDir> : デフォルト: 設定ファイルまたは package.json を含むルートディレクトリ。あるいは、もし package.json が見つからなければ、pwd の結果を設定します。
 	},
-	moduleFileExtensions: ["js"] // テスト対象の拡張子を列挙する
+	moduleFileExtensions: ["js"], // テスト対象の拡張子を列挙する
+	testMatch: [
+		// テスト対象のファイル名にマッチする正規表現文字列の配列 @see https://app.codegrid.net/entry/2018-jest-1
+		// "**/__tests__/**/*.[jt]s?(x)",
+		"**/?(*.)+(spec|test).[jt]s?(x)"
+	],
+	testPathIgnorePatterns: ["/node_modules/", "**/__tests__/**/*.[jt]s?(x)"]
 };
