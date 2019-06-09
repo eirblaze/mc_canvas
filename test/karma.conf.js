e4dddd1ef2cdd8586cd3f393af38fa5360e7bf68
eirblaze <eirblaze@live.jp>
2019-06-09 21:55:30 +0900

    独自テストと自動自動テストを両立させてみるテスト

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,45 +1,60 @@
 // Configuration for karma
 module.exports = function(config) {
-	config.set({
-		logLevel: config.LOG_DEBUG,
-		browserNoActivityTimeout: 60000,
-		browserDisconnectTimeout: 10000,
-		browsers: ["ChromeHeadless"],
-		frameworks: ["mocha", "snapshot", "mocha-snapshot"],
-		reporters: ["mocha"],
-		preprocessors: {
-			"**/__snapshots__/**/*.md": ["snapshot"],
-			"__tests__/index.js": ["webpack", "sourcemap"],
-			"__tests__/**/*.json": ["json"]
-		},
-		files: [
-			"**/__snapshots__/**/*.md",
-			"../Outputs/CanvasMasao.js",
-			{
-				pattern: "../Samples/*.gif",
-				watched: false,
-				included: false
+	var config_set_args = {};
+
+	if (config.mode === "automasao") {
+		config_set_args = {
+			// logLevel: config.LOG_DEBUG,
+			browserNoActivityTimeout: 60000,
+			browserDisconnectTimeout: 10000,
+			browsers: ["ChromeHeadless"],
+			frameworks: ["mocha", "snapshot", "mocha-snapshot"],
+			reporters: ["mocha"],
+			preprocessors: {
+				"**/__snapshots__/**/*.md": ["snapshot"],
+				"__tests__/index.js": ["webpack", "sourcemap"],
+				"__tests__/**/*.json": ["json"]
 			},
-			"__tests__/**/*.masao.json",
-			"__tests__/index.js"
-		],
-		client: {
-			captureConsole: true,
-			mocha: {
-				timeout: 60000
+			files: [
+				"**/__snapshots__/**/*.md",
+				"../Outputs/CanvasMasao.js",
+				{
+					pattern: "../Samples/*.gif",
+					watched: false,
+					included: false
+				},
+				"__tests__/**/*.masao.json",
+				"__tests__/index.js"
+			],
+			client: {
+				captureConsole: true,
+				mocha: {
+					timeout: 60000
+				}
+			},
+			mochaReporter: {
+				showDiff: !!process.env.DIFF
+			},
+			// setting for mocha-snapshot
+			snapshot: {
+				update: !!process.env.UPDATE,
+				prune: !!process.env.PRUNE
+			},
+			// setting for karma-webpack
+			webpack: {
+				mode: "development"
 			}
-		},
-		mochaReporter: {
-			showDiff: !!process.env.DIFF
-		},
-		// setting for mocha-snapshot
-		snapshot: {
-			update: !!process.env.UPDATE,
-			prune: !!process.env.PRUNE
-		},
-		// setting for karma-webpack
-		webpack: {
-			mode: "development"
-		}
-	});
+		};
+	}
+
+	// 独自テスト
+	if (config.mode === "eb-jasmine") {
+		config_set_args = {
+			logLevel: config.LOG_DEBUG,
+			frameworks: ["jasmine"]
+		};
+	}
+
+	config.set(config_set_args);
+	//console.log(config);
 };
