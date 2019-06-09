04c15eb0d9c5bcf619607f5fe48947e6c5b0309b
eirblaze <eirblaze@live.jp>
2019-06-10 01:29:45 +0900

    別形式のエクスポートも試す

--- test-eb/add.test.js
+++ test-eb/add.test.js
@@ -1,8 +1,8 @@
-import add from "../Sources/demoApp";
+import * as demoApp from "../Sources/demoApp";
 
 describe("Counter", () => {
 	test("adds 1 + 2 to equal 3", () => {
 		// Jest独特の記述にtestというものもあります。これはitの別名です。
-		expect(add(1, 2)).toBe(3);
+		expect(demoApp.add(1, 2)).toBe(3);
 	});
 });
