844c9c81183fee1f1a8042d8f20901d757bc7707
eirblaze <eirblaze@live.jp>
2019-06-10 01:52:36 +0900

    describeの動きをみる

--- test-eb/add.test.js
+++ test-eb/add.test.js
@@ -1,12 +1,13 @@
 import demoApp from "../Sources/demoApp";
 
-describe("Counter", () => {
+describe("グループ", () => {
 	test("1 + 2 は 3 になる", () => {
 		// Jest独特の記述にtestというものもあります。これはitの別名です。
 		expect(demoApp.add(1, 2)).toBe(3);
 	});
-	test("1 - 2 は -1 になる", () => {
-		// Jest独特の記述にtestというものもあります。これはitの別名です。
-		expect(demoApp.sub(1, 2)).toBe(-1);
+	describe("子グループ", () => {
+		test("1 - 2 は -1 になる", () => {
+			expect(demoApp.sub(1, 2)).toBe(-1);
+		});
 	});
 });
