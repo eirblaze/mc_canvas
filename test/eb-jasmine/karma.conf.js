042c1b3398c757825a12c93c3d48fa82c436637b
eirblaze <eirblaze@live.jp>
2019-06-09 23:59:36 +0900

    とりあえず見やすくする

--- test/eb-jasmine/karma.conf.js
+++ test/eb-jasmine/karma.conf.js
@@ -1,9 +1,11 @@
 // Configuration for karma
 module.exports = function(config) {
-	config.set({
+	var cfg_arg = {
 		logLevel: config.LOG_DEBUG,
 		frameworks: ["jasmine"],
 		basePath: "../..",
 		files: ["Sources/demoApp.js", "test/eb-jasmine/*Spec.js"]
-	});
+	};
+
+	return config.set(cfg_arg);
 };
