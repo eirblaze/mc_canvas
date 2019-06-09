c4093cf0bd478c1eca5ac971e54c77e440687a1f
eirblaze <eirblaze@live.jp>
2019-06-10 02:04:34 +0900

    共通処理で、初期値はtestのたびに1になる。

--- test-eb/demo.test.js
+++ test-eb/demo.test.js
@@ -2,18 +2,25 @@ import Counter from "../Sources/demoApp";
 
 describe("カウンター", () => {
 	let counter;
+	const init_value = 1;
 	beforeEach(() => {
 		// 共通前処理を試す
-		counter = new Counter(1);
+		counter = new Counter(init_value);
 	});
 	describe("increment()", () => {
-		test("1 をふやすと 2 になる", () => {
+		test(init_value + " をふやすと 2 になる", () => {
 			counter.increment();
 			expect(counter.count).toBe(2);
 		});
+		test("2回 ふやすと 3 になる", () => {
+			// 共通処理で、初期値はtestのたびに1になる。
+			counter.increment();
+			counter.increment();
+			expect(counter.count).toBe(3);
+		});
 	});
 	describe("decrement()", () => {
-		test("1 をへらすと 0 になる", () => {
+		test(init_value + " をへらすと 0 になる", () => {
 			counter.decrement();
 			expect(counter.count).toBe(0);
 		});
